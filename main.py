dde4bcff194fac36c07d4da2bfa1fef9bb031fb3
Tanmay Patil <patil.tanmay0697@gmail.com>
2025-02-07 14:34:14 -0800

    Add processing logic for text file

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from typing import List
 def read_flow_logs(log_file: str) -> str:  
     with open(log_file, "r") as file:
         content = file.read()
-    # print(content.split("\n"))
-    preprocess_logs(content)
     return content
 
 def preprocess_logs(content: str) -> List[str]:
     lines = content.split("\n")
     processed_lines = [l.strip() for l in lines]
     return processed_lines
+
+# def parse_logs(logs: List[str]) -> 
     
     
 
 if __name__ == "__main__":
-    read_flow_logs("./data/input/flow_logs.txt")
+    content = read_flow_logs("./data/input/flow_logs.txt")
+    lines = preprocess_logs(content=content)
+    for line in lines:
+        print(line.split())
